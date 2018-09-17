28e3e9e551ee4e5e426e4cff4c60aedd77e4f9af
Lorién <lorienportella@gmail.com>
2018-09-18 00:44:19 +0200

    cleaned the new code

--- Edgyne/ModuleImGui.cpp
+++ Edgyne/ModuleImGui.cpp
@@ -15,13 +15,7 @@ ModuleImGui::ModuleImGui(Application* app, bool start_enabled) : Module(app, sta
 {
 }
 
-// dear imgui: standalone example application for SDL2 + OpenGL
-// If you are new to dear imgui, see examples/README.txt and documentation at the top of imgui.cpp.
-// (SDL is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan graphics context creation, etc.)
 
-// **DO NOT USE THIS CODE IF YOUR CODE/ENGINE IS USING MODERN OPENGL (SHADERS, VBO, VAO, etc.)**
-// **Prefer using the code in the example_sdl_opengl3/ folder**
-// See imgui_impl_sdl.cpp for details.
 
 bool ModuleImGui::Init()
 {
@@ -58,20 +52,7 @@ update_status ModuleImGui::PreUpdate(float dt)
 update_status ModuleImGui::Update(float dt)
 {
 	update_status status = UPDATE_CONTINUE;
-	//ImGui::SetNextWindowPos({ 0,0 });
-	//ImGuiWindowFlags window_flags = 0;
-	//window_flags |= ImGuiWindowFlags_NoTitleBar;
-	//window_flags |= ImGuiWindowFlags_NoMove;
-	//window_flags |= ImGuiWindowFlags_NoResize;
-	//window_flags |= ImGuiWindowFlags_NoCollapse;
-	//ImGui::Begin("Test Menu", 0, window_flags);
-	//ImGui::SetWindowSize({ 85, 35 });
-	//ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor(255, 0, 0, 255));
-	//ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor(150, 0, 0, 255));
-	//ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor(100, 0, 0, 255));
-	//
-	//ImGui::PopStyleColor(3);
-	//ImGui::End();
+
 	
 
 	
@@ -103,14 +84,10 @@ update_status ModuleImGui::Update(float dt)
 
 update_status ModuleImGui::PostUpdate(float dt)
 {
-	//ImGui::Render();
-	//ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
 	
 	ImGui::Render();
-	//glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-	//glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-	//glClear(GL_COLOR_BUFFER_BIT);
-	//glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound
+	
 	ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
 	update_status status = UPDATE_CONTINUE;
 	if (to_close == true)
@@ -120,45 +97,6 @@ update_status ModuleImGui::PostUpdate(float dt)
 	return status;
 }
 
-
-
-
-
-							   // Setup Dear ImGui binding
-	
-	//ImGui::StyleColorsClassic();
-
-	// Load Fonts
-	// - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them. 
-	// - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple. 
-	// - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-	// - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-	// - Read 'misc/fonts/README.txt' for more instructions and details.
-	// - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-	//io.Fonts->AddFontDefault();
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-	//ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-	//IM_ASSERT(font != NULL);
-
-
-
-	// Main loop
-
-
-	// Cleanup
-//	ImGui_ImplOpenGL2_Shutdown();
-//	ImGui_ImplSDL2_Shutdown();
-//	ImGui::DestroyContext();
-//
-//	SDL_GL_DeleteContext(gl_context);
-//	SDL_DestroyWindow(window);
-//	SDL_Quit();
-//
-//	return 0;
-//}
 
 ModuleImGui::~ModuleImGui()
 {
