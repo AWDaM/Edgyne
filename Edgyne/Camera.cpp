1a2ff50d736200b629357c8bc34096b92f99b169
Lorién <lorienportella@gmail.com>
2018-11-04 19:04:17 +0100

    minor fixes

--- Edgyne/Camera.cpp
+++ Edgyne/Camera.cpp
@@ -160,5 +160,5 @@ void Camera::ChangeFrustum()
 	frustum.farPlaneDistance = 1000.0f;
 
 	frustum.verticalFov = DEGTORAD * 90.0f;
-	frustum.horizontalFov = 2.f * Atan(Tan(frustum.verticalFov*0.5f)*16.0f / 9.0f);
+	frustum.horizontalFov = 2.f * Atan(Tan(frustum.verticalFov*0.5f)*(16.0f / 9.0f));
 }
