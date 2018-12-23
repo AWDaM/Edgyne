495f354ca919b406b65763f5573f9d7d14cd8fb1
Lorién <lorienportella@gmail.com>
2018-12-23 21:53:21 +0100

    quickfix that really doesnt fix anything

--- Edgyne/Mesh.cpp
+++ Edgyne/Mesh.cpp
@@ -224,21 +224,44 @@ bool Mesh::ComponentDraw()
 			if (shader_program)
 			{
 				glUseProgram(shader_program->program);
+				GLint model_loc = glGetUniformLocation(shader_program->program, "model_matrix");
+				glUniformMatrix4fv(model_loc, 1, GL_FALSE, *(game_object->global_transform_matrix.Transposed()).v);
+				GLint proj_loc = glGetUniformLocation(shader_program->program, "projection");
+				glUniformMatrix4fv(proj_loc, 1, GL_FALSE, App->camera->editor_camera->GetProjectionMatrix());
+				GLint view_loc = glGetUniformLocation(shader_program->program, "view");
+				glUniformMatrix4fv(view_loc, 1, GL_FALSE, App->camera->editor_camera->GetViewMatrix());
+				GLint time_loc = glGetUniformLocation(shader_program->program, "time");
+				glUniform1f(time_loc, App->time_manager->game_clock_seconds);
 			}
 			else
+			{
 				glUseProgram(App->shaders->defaultProgram->program);
+
+				GLint model_loc = glGetUniformLocation(App->shaders->defaultProgram->program, "model_matrix");
+				glUniformMatrix4fv(model_loc, 1, GL_FALSE, *(game_object->global_transform_matrix.Transposed()).v);
+				GLint proj_loc = glGetUniformLocation(App->shaders->defaultProgram->program, "projection");
+				glUniformMatrix4fv(proj_loc, 1, GL_FALSE, App->camera->editor_camera->GetProjectionMatrix());
+				GLint view_loc = glGetUniformLocation(App->shaders->defaultProgram->program, "view");
+				glUniformMatrix4fv(view_loc, 1, GL_FALSE, App->camera->editor_camera->GetViewMatrix());
+				GLint time_loc = glGetUniformLocation(App->shaders->defaultProgram->program, "time");
+				glUniform1f(time_loc, App->time_manager->game_clock_seconds);
+			}
 		}
 		else
+		{
 			glUseProgram(App->shaders->defaultProgram->program);
 
-		GLint model_loc = glGetUniformLocation(App->shaders->defaultProgram->program, "model_matrix");
-		glUniformMatrix4fv(model_loc, 1, GL_FALSE, *(game_object->global_transform_matrix.Transposed()).v);
-		GLint proj_loc = glGetUniformLocation(App->shaders->defaultProgram->program, "projection");
-		glUniformMatrix4fv(proj_loc, 1, GL_FALSE, App->camera->editor_camera->GetProjectionMatrix());
-		GLint view_loc = glGetUniformLocation(App->shaders->defaultProgram->program, "view");
-		glUniformMatrix4fv(view_loc, 1, GL_FALSE, App->camera->editor_camera->GetViewMatrix());
-		GLint time_loc = glGetUniformLocation(App->shaders->defaultProgram->program, "time");
-		glUniform1f(time_loc, App->time_manager->game_clock_seconds);
+			GLint model_loc = glGetUniformLocation(App->shaders->defaultProgram->program, "model_matrix");
+			glUniformMatrix4fv(model_loc, 1, GL_FALSE, *(game_object->global_transform_matrix.Transposed()).v);
+			GLint proj_loc = glGetUniformLocation(App->shaders->defaultProgram->program, "projection");
+			glUniformMatrix4fv(proj_loc, 1, GL_FALSE, App->camera->editor_camera->GetProjectionMatrix());
+			GLint view_loc = glGetUniformLocation(App->shaders->defaultProgram->program, "view");
+			glUniformMatrix4fv(view_loc, 1, GL_FALSE, App->camera->editor_camera->GetViewMatrix());
+			GLint time_loc = glGetUniformLocation(App->shaders->defaultProgram->program, "time");
+			glUniform1f(time_loc, App->time_manager->game_clock_seconds);
+		}
+
+		
 
 
 		glBindVertexArray(resource_mesh->VAO);
